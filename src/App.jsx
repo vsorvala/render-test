a0e21d677ebca4444958bad6a169dd72f3b039d2
vmsorvala <vmsorvala@gmail.com>
2024-10-19 01:21:14 +0300

    1. palautus 19.10.2024

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  const friends = [
+    { name: 'Leevi', age: 4 },
+    { name: 'Venla', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+export default App
