25624370649dccbb48873f7544b25d87a632a0ab
vmsorvala <vmsorvala@gmail.com>
2024-11-12 20:30:21 +0200

    harj1

--- src/osa1/kurssitiedot/kurssitiedot/src/App.jsx
+++ src/osa1/kurssitiedot/kurssitiedot/src/App.jsx
@@ -1,4 +1,44 @@
 const App = () => {
+
+
+  const Header = () => {
+    return (
+      <div>
+        <h1>{course}</h1>
+      </div>
+
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <>
+         <p>
+          {props.part} {props.exercises}
+        </p>
+      </>
+
+    )
+  }
+
+  const Content = () => {
+    return (
+      <>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
+      </>
+    )
+
+  }
+  const Total = () => {
+    return (
+      <>
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      </>
+
+    )
+  }
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -8,18 +48,12 @@ const App = () => {
   const exercises3 = 14
 
   return (
+
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content />
+      <Total />
+
     </div>
   )
 }
